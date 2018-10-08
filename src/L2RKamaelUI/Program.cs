4a8e4756e13330a7812f98baf4b0df59c5051971
Bekim Gjokaj <bekim.gjokaj@medmen.com>
2018-10-07 23:03:21 -0700

    Initial push

---
+++ src/L2RKamaelUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kamael.UI
+{
+    
+
+    internal static class Kamael
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Globals.args = args;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ src/L2RKamaelUI/frmMain.cs
@@ -0,0 +1,49 @@
+﻿using Kamael.Packets;
+using System;
+using System.Windows.Forms;
+using SharpPcap;
+
+namespace Kamael.UI
+{
+    public partial class frmMain : Form
+    {
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+
+            // Wait for 'Enter' from the user.
+            //Console.ReadLine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Stop the capturing process
+            device.StopCapture();
+            Console.WriteLine("-- Capture stopped.");
+
+            // Print out the device statistics
+            Console.WriteLine(device.Statistics.ToString());
+
+            // Close the pcap device
+            device.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tmpStr = "";
+            for (int i = 0; i <= 11025; i++)
+            {
+                tmpStr += string.Format("case {0}: break;//return new \r\n", i);
+                Application.DoEvents();
+            }
+
+            txtLog.Text = tmpStr;
+        }
+    }
+}
